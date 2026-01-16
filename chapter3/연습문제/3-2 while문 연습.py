3308633e6219e53e7c4c1782749664b114c7e690
imjiyeon <48472349+imjiyeon@users.noreply.github.com>
2026-01-16 09:15:52 +0900

    Refactor increment operations in while loops
    Replaced increment operator with explicit assignment for clarity.

--- chapter3/연습문제/3-2 while문 연습.py
+++ chapter3/연습문제/3-2 while문 연습.py
@@ -2,39 +2,38 @@
 i = 0
 while i < 3:
     print("하이")
-    i += 1
+    i = i + 1
 
 # 2. 반복문을 사용해 "경고!"를 5번 출력하세요
 i = 0
 while i < 5:
     print("경고!")
-    i += 1    
+    i = i + 1   
 
 # 3. 반복문을 사용해 "연습을 1번 완료했습니다"부터
 # "연습을 5번 완료했습니다"까지 출력하세요
 i = 1
 while i <= 5:
     print(f"연습을 {i}번 완료했습니다")
-    i += 1
+    i = i + 1
 
-#
 # 4. 반복문을 사용해 0부터 4까지 출력하세요
 i = 0
 while i < 5:
     print(i)
-    i += 1
+    i = i + 1
 
 # 5. 반복문을 사용해 11부터 20까지 출력하세요
 i = 11
 while i <= 20:
     print(i)
-    i += 1
+    i = i + 1
 
 # 6. 반복문을 사용해 5부터 1까지 출력하세요
 i = 5
 while i >= 1:
     print(i)
-    i -= 1    
+    i = i - 1   
 
 # 7. 반복문을 사용해 "*"를 5번 이어서 출력하세요
 # 결과: "*****"
@@ -43,11 +42,10 @@ result = ""
 
 while i < 5:
     result += "*" # 문자열 누적
-    i += 1
+    i = i + 1
 
 print(result)
 
-#
 # 8. 아래와 같이 리스트를 만들고 
 # 반복문을 사용해 요소를 하나씩 출력하세요
 lis = ['a','b','c','d','e']
@@ -72,7 +70,7 @@ hap = 0
 
 while i < 5:
     hap = hap + lis[i]
-    i += 1
+    i = i + 1
 
 print("모든 요소의 합:", hap)
 
@@ -86,6 +84,6 @@ hap = 0
 while i < 6:
     if(type(lis[i])==int or type(lis[i])==float):
         hap = hap + lis[i]
-    i += 1
+    i = i + 1
 
 print(hap)
